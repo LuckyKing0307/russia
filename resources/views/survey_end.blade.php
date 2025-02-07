a690310166bacab07a84c2dcd833a169d9c019f6
LuckyKing0307 <jasaergasheva@gmail.com>
2025-02-07 08:05:53 +0500

    Upload server

--- resources/views/survey_end.blade.php
+++ resources/views/survey_end.blade.php
@@ -16,6 +16,24 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1416343942675898');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=1416343942675898&ev=PageView&noscript=1"
+        /></noscript>
+    <!-- End Meta Pixel Code -->
+
     <style>
         body{
             background-image: none;
@@ -61,7 +79,7 @@
             Bepul darsda qatnashish  <br>uchun, yopiq telegram  <br>kanalimizga obuna bo'ling!
         </div>
         <img src="{{url('/img/link.svg')}}" class="link">
-        <a class="reg_btn end_btn_reg" href="" target="_blank">Kanalga qo’shilish</a>
+        <a class="reg_btn end_btn_reg" href="https://t.me/+7mKPFzWw3JQ4MmY6" target="_blank">Kanalga qo’shilish</a>
         <img src="{{url('/img/img_4.png')}}" alt="" class="chel_btn">
     </div>
 </div>
