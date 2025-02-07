989f7f7c5afe5e246d9c2dedb31502215aa9bd75
LuckyKing0307 <jasaergasheva@gmail.com>
2025-02-07 20:24:35 +0500

    Upload server

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Naimix Xususiy Bandlik Agentligi</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
