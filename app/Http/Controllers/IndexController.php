fe74c2d5e92ad724b8a2abf010bffc1dae8e8957
LuckyKing0307 <jasaergasheva@gmail.com>
2025-02-08 09:55:39 +0500

    Upload server

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -14,15 +14,23 @@ class IndexController extends Controller
 
     public function index()
     {
-        if (Session::getId()){
+        $userIP = $this->getUserIP();
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        $fbp = $_COOKIE['_fbp'] ?? null;
+        $fbc = $_COOKIE['_fbc'] ?? null;
+        if (Session::getId()) {
             $id = Session::getId();
-            $response = Http::post("https://graph.facebook.com/v18.0/".env('FACEBOOK_PIXEL_ID')."/events", [
+            $response = Http::post("https://graph.facebook.com/v18.0/" . env('FACEBOOK_PIXEL_ID') . "/events", [
                 'data' => [
                     [
                         'event_name' => 'ViewContent',
                         'event_time' => time(),
                         'user_data' => [
-                            "external_id"=>$id
+                            "external_id" => $id,
+                            'client_ip_address' => $userIP, // IP
+                            'client_user_agent' => $userAgent, // UserAgent
+                            'fbp' => $fbp,
+                            'fbc' => $fbc,
                         ],
                         'custom_data' => [
                             'content_ids' => '0',
@@ -33,14 +41,18 @@ class IndexController extends Controller
                 ],
                 'access_token' => env('FACEBOOK_ACCESS_TOKEN')
             ]);
-        }else{
-            $response = Http::post("https://graph.facebook.com/v18.0/".env('FACEBOOK_PIXEL_ID')."/events", [
+        } else {
+            $response = Http::post("https://graph.facebook.com/v18.0/" . env('FACEBOOK_PIXEL_ID') . "/events", [
                 'data' => [
                     [
                         'event_name' => 'ViewContent',
                         'event_time' => time(),
                         'user_data' => [
-                            "external_id"=>"new_user"
+                            "external_id" => "new_user",
+                            'client_ip_address' => $userIP, // IP
+                            'client_user_agent' => $userAgent, // UserAgent
+                            'fbp' => $fbp,
+                            'fbc' => $fbc,
                         ],
                         'custom_data' => [
                             'content_ids' => '0',
@@ -64,7 +76,12 @@ class IndexController extends Controller
 
         $session = Session::put('survey_completed', true);
 
-        $response = Http::post("https://graph.facebook.com/v18.0/".env('FACEBOOK_PIXEL_ID')."/events", [
+        $userIP = $this->getUserIP();
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+        $fbp = $_COOKIE['_fbp'] ?? null;
+        $fbc = $_COOKIE['_fbc'] ?? null;
+        $response = Http::post("https://graph.facebook.com/v18.0/" . env('FACEBOOK_PIXEL_ID') . "/events", [
             'data' => [
                 [
                     'event_name' => 'Lead',  // Событие "Лид" (заполнение формы)
@@ -74,6 +91,10 @@ class IndexController extends Controller
                     'user_data' => [
                         'fn' => [hash('sha256', strtolower(trim($data['name'])))],  // Email пользователя (хешируем SHA-256)
                         'ph' => [hash('sha256', preg_replace('/\D/', '', $data['phone']))], // Телефон (удаляем всё кроме цифр)
+                        'client_ip_address' => $userIP, // IP
+                        'client_user_agent' => $userAgent, // UserAgent
+                        'fbp' => $fbp,
+                        'fbc' => $fbc,
                     ],
                     'custom_data' => [
                         'form_id' => $user->id,  // ID формы (например, "Форма обратной связи")
@@ -89,5 +110,16 @@ class IndexController extends Controller
     public function download()
     {
         return Excel::download(new RegistredExport, 'users.xlsx');
+    }
+
+    function getUserIP()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            return $_SERVER['REMOTE_ADDR'];
+        }
     }
 }
